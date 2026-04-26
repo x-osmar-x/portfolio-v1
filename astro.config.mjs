56ede1f98100af78764d46533d1e6c8d2f8d0183
Osmar Barrientos Chuveyzer <o1b9c9h3@gmail.com>
2026-04-25 20:05:38 -0400

    fix: configure astro for static build with node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://x-osmar-x.github.io',
+  base: '/portfolio-v1',
   vite: {
     plugins: [tailwindcss()],
   },
+  output: 'static',
+  adapter: node({
+    mode: 'standalone'
+  }),
 });
